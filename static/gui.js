79419c7b1b7af6c41ffdf24249a4e9f03f32d802
Carson Fleming <cflems@cflems.net>
2017-09-08 14:57:40 -0400

    Nevermind, that didn't work.

--- static/gui.js
+++ static/gui.js
@@ -2,8 +2,5 @@ $(function () {
   $('#inphash').click(function (e) {
     let $pass = $('#inppass');
     $pass.val(wp_hash_password($pass.val()));
-    $pass.focus(function (ev) {
-      $pass.select();
-    });
   });
 });
