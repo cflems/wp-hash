2ea69aaabb1e3cb404501f815f2572189d1498d7
Carson Fleming <cflems@cflems.net>
2017-09-08 14:56:41 -0400

    Add select all functionality to gui

--- static/gui.js
+++ static/gui.js
@@ -2,5 +2,8 @@ $(function () {
   $('#inphash').click(function (e) {
     let $pass = $('#inppass');
     $pass.val(wp_hash_password($pass.val()));
+    $pass.focus(function (ev) {
+      $pass.select();
+    });
   });
 });
